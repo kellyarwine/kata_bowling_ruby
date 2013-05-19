db75058bc5516d1ab60d219e1efdb648bc381d36
Takayuki Goto <tak.yuki@gmail.com>
2013-05-19 12:38:02 -0500

    Update game_spec.rb
    I was really bored, and your kata was the first thing I saw on my github feed. I took out like 60 lines of code, was fun!

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,97 +1,58 @@
 require 'game'
 
+def add_all scores
+  game = Game.new
+  scores.each do |score|
+    game.add(score)
+  end
+  game
+end
+
 describe Game do
-
-  let(:game) { Game.new }
-
   it "returns a score of 5 when 5 pins are input" do
-    game.add(5)
-    game.add(4)
-    game.calculate_score.should == 9
+    add_all([5, 4]).calculate_score.should == 9
+  end
+
+  it "returns the score of a perfect game" do
+    game = add_all([1, 4, 4, 5, 6, 4, 5, 5, 10, 0,
+                    1, 7, 3, 6, 4, 10, 2, 8, 6])
+
+    game.calculate_score.should == 133
+  end
+
+  it "test end of array" do
+    add_all((Array.new(18){0}).push(2, 8, 10)).calculate_score.should == 20
   end
 
   it "returns a score and score for frame for 4 throws" do
-    game.add(5)
-    game.add(4)
-    game.add(7)
-    game.add(2)
+    game = add_all([5, 4, 7, 2])
     game.calculate_score.should == 18
     game.scorer.score_for_frame(1).should == 9
     game.scorer.score_for_frame(2).should == 18
   end
 
   it "returns a score when there is a spare" do
-    game.add(3)
-    game.add(7)
-    game.add(3)
-    game.scorer.score_for_frame(1).should == 13
+    add_all([3, 7, 3]).scorer.score_for_frame(1).should == 13
   end
 
   it "returns a score of 10 plus the next frame when there is a spare" do
-    game.add(3)
-    game.add(7)
-    game.add(3)
-    game.add(2)
+    game = add_all([3, 7, 3, 2])
     game.scorer.score_for_frame(1).should == 13
     game.scorer.score_for_frame(2).should == 18
     game.calculate_score.should == 18
   end
 
   it "returns a score of 10 plus the next 2 frames when there is a strike" do
-    game.add(10)
-    game.add(3)
-    game.add(6)
+    game = add_all([10, 3, 6])
     game.scorer.score_for_frame(1).should == 19
     game.scorer.score_for_frame(2).should == 28
     game.calculate_score.should == 28
   end
 
   it "returns the score of a perfect game" do
-    12.times do
-      game.add(10)
-    end
-
+    game = add_all(Array.new(12) {10})
     game.scorer.score_for_frame(1).should == 30
     game.scorer.score_for_frame(2).should == 60
     game.calculate_score.should == 300
   end
-
-  it "test end of array" do
-    9.times do
-      game.add(0)
-      game.add(0)
-    end
-
-    game.add(2)
-    game.add(8)
-    game.add(10)
-
-    game.calculate_score.should == 20
-  end
-
-  it "returns the score of a perfect game" do
-    game.add(1)
-    game.add(4)
-    game.add(4)
-    game.add(5)
-    game.add(6)
-    game.add(4)
-    game.add(5)
-    game.add(5)
-    game.add(10)
-    game.add(0)
-    game.add(1)
-    game.add(7)
-    game.add(3)
-    game.add(6)
-    game.add(4)
-    game.add(10)
-    game.add(2)
-    game.add(8)
-    game.add(6)
-
-    game.calculate_score.should == 133
-  end
-
-
 end
